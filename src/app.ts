522b9579b7b5db6783db7b173897f9b7cb4cf4fa
Seung Ju <kingsky32@gmail.com>
2021-12-10 13:58:39 +0900

    check requestbody

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const app: express.Application = express();
 app.use(logger('dev'));
 
 app.post('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log(JSON.stringify(req));
   res.send('Done');
 });
 
