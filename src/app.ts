cd03316466ceef3821496d4b7050739a83b08274
Seung Ju <kingsky32@gmail.com>
2021-12-10 13:28:23 +0900

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import * as logger from 'morgan';
+
+const PORT = process.env.PORT ?? 3000;
+
+const app: express.Application = express();
+
+app.use(logger('dev'));
+
+app.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.send('Done');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
+
+export default app;
