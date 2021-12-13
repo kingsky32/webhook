0fe5b259ff48d91cfb61959092dd74ce6e7bfde1
Seung Ju <kingsky32@gmail.com>
2021-12-13 13:34:38 +0900

    Setup BodyParser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
 
 const PORT = process.env.PORT ?? 3000;
 
 const app: express.Application = express();
 
 app.use(logger('dev'));
+app.use(express.json());
 
 app.post('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
   console.log(req);
